2b4eabde9ebb93f844e7b5ea6637da6a053e9ef8
BEDINADE Dadja <ebedinade@gmail.com>
2026-02-21 11:05:44 +0000

    Initial commit

---
+++ apex/workspace/DEMO_RESTO.sql
@@ -0,0 +1,281 @@
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- Oracle APEX export file
+--
+-- You should run this script using a SQL client connected to the database as
+-- the owner (parsing schema) of the application or as a database user with the
+-- APEX_ADMINISTRATOR_ROLE role.
+--
+-- This export file has been automatically generated. Modifying this file is not
+-- supported by Oracle and can lead to unexpected application and/or instance
+-- behavior now or in the future.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_imp.import_begin (
+ p_version_yyyy_mm_dd=>'2024.11.30'
+,p_default_workspace_id=>15475120125710231
+);
+end;
+/
+prompt  WORKSPACE 15475120125710231
+--
+-- Workspace, User Group, User, and Team Development Export:
+--   Date and Time:   10:50 Samedi Février 21, 2026
+--   Exported By:     ADMIN
+--   Export Type:     Workspace Export
+--   Version:         24.2.9
+--   Instance ID:     743328252654632
+--
+-- Import:
+--   Using Instance Administration / Manage Workspaces
+--   or
+--   Using SQL*Plus as the Oracle user APEX_240200
+ 
+begin
+    wwv_flow_imp.set_security_group_id(p_security_group_id=>15475120125710231);
+end;
+/
+----------------
+-- W O R K S P A C E
+-- Creating a workspace will not create database schemas or objects.
+-- This API creates only the meta data for this APEX workspace
+prompt  Creating workspace DEMO_RESTO...
+begin
+wwv_flow_fnd_user_api.create_company (
+  p_id => 15475237695710245
+ ,p_provisioning_company_id => 15475120125710231
+ ,p_short_name => 'DEMO_RESTO'
+ ,p_display_name => 'DEMO_RESTO'
+ ,p_first_schema_provisioned => 'RESTOADMIN'
+ ,p_company_schemas => 'RESTOADMIN'
+ ,p_account_status => 'ASSIGNED'
+ ,p_allow_plsql_editing => 'Y'
+ ,p_allow_app_building_yn => 'Y'
+ ,p_allow_packaged_app_ins_yn => 'Y'
+ ,p_allow_sql_workshop_yn => 'Y'
+ ,p_allow_team_development_yn => 'Y'
+ ,p_allow_to_be_purged_yn => 'Y'
+ ,p_allow_restful_services_yn => 'Y'
+ ,p_source_identifier => 'DEMO_RES'
+ ,p_webservice_logging_yn => 'Y'
+ ,p_path_prefix => 'DEMO_RESTO'
+ ,p_files_version => 1
+ ,p_is_extension_yn => 'N'
+ ,p_env_banner_yn => 'N'
+ ,p_env_banner_pos => 'LEFT'
+);
+end;
+/
+----------------
+-- G R O U P S
+--
+prompt  Creating Groups...
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1466164091743656,
+  p_GROUP_NAME => 'OAuth2 Client Developer',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to register OAuth2 Client Applications');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1466031728743656,
+  p_GROUP_NAME => 'RESTful Services',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to use RESTful Services with this workspace');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1465928717743654,
+  p_GROUP_NAME => 'SQL Developer',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to use SQL Developer with this workspace');
+end;
+/
+prompt  Creating group grants...
+----------------
+-- U S E R S
+-- User repository for use with APEX cookie-based authentication.
+--
+prompt  Creating Users...
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '15494329116742734',
+  p_user_name                    => 'RESTO',
+  p_first_name                   => 'Dadja',
+  p_last_name                    => 'BEDINADE',
+  p_description                  => '',
+  p_email_address                => 'adminopn@ntcinet.com',
+  p_web_password                 => '1EB96714AB909C16EDE1234F446499E78E8B214AD42B1FE0AFD672052649B7149F3D2C85C392E630C27FA22D30CF7C1EB70D331CF8F7EEF969172EB0F6BA9191',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '',
+  p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
+  p_default_schema               => 'RESTOADMIN',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('202602080000','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 0,
+  p_change_password_on_first_use => 'N',
+  p_first_password_use_occurred  => 'N',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '15475042759710231',
+  p_user_name                    => 'RESTOADMIN',
+  p_first_name                   => 'Dadja',
+  p_last_name                    => 'BEDINADE',
+  p_description                  => '',
+  p_email_address                => 'adminopn@ntcinet.com',
+  p_web_password                 => 'CB3276D2F498563A9C2B9E8DA74E416B9691898F37557845DBA045B7BFC1C2322EDEB27594CEC36384B52CC7F82A3CCCAE269326DB77846794F50ACF6D761124',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '',
+  p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
+  p_default_schema               => 'RESTOADMIN',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('202602081625','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 0,
+  p_change_password_on_first_use => 'Y',
+  p_first_password_use_occurred  => 'Y',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+---------------------------
+-- D G  B L U E P R I N T S
+-- Creating Data Generator Blueprints...
+prompt Check Compatibility...
+begin
+-- This date identifies the minimum version required to import this file.
+wwv_flow_team_api.check_version(p_version_yyyy_mm_dd=>'2010.05.13');
+end;
+/
+ 
+begin wwv_flow.g_import_in_progress := true; wwv_flow.g_user := USER; end; 
+/
+ 
+--
+prompt ...feedback
+--
+begin
+null;
+end;
+/
+--
+prompt ...Issue Templates
+--
+begin
+null;
+end;
+/
+--
+prompt ...Issue Email Prefs
+--
+begin
+null;
+end;
+/
+--
+prompt ...Label Groups
+--
+begin
+null;
+end;
+/
+--
+prompt ...Labels
+--
+begin
+null;
+end;
+/
+--
+prompt ... Milestones
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issue Attachments
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Milestones
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Labels
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues stakeholders
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Comments
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Events
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Notifications
+--
+begin
+null;
+end;
+/
+ 
+prompt ... Extension Links
+ 
+ 
+prompt ... Extension Grants
+ 
+begin
+wwv_flow_imp.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false)
+);
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
